995c29ba23b54d5db163bf281d1738b29d79657c
PERFICIENT\p.palandurkar <p.palandurkar@perficient.com>
2022-01-27 15:15:37 +0530

    component life cycle

---
+++ src/ComponentLifeCycle.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class ComponentLifeCycle extends React.Component {
+
+    state = {
+        count: 0
+    };
+
+    componentDidMount() {
+        console.log('in mount');
+    }
+
+    componentDidUpdate() {
+        console.log('in upate');
+    }
+
+    render() {
+        const { count: cnt } = this.state;
+        console.log('in rener');
+        return (<div>
+            <button onClick={() => this.setState({count: cnt + 1})}>click me</button>
+            <p>{ count }</p>
+        </div>);
+    }
+}
+
+export default ComponentLifeCycle;
+
