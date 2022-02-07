faabf3a697fbeb65569e130191161d8e5193eb74
PERFICIENT\p.palandurkar <p.palandurkar@perficient.com>
2022-02-07 20:57:07 +0530

    chilDren props

---
+++ src/ChildrenProps.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class ChildrenProps extends React.Component {
+    /*
+        <ChildrenProps>
+            <div>
+                <div>hi1</div>
+                <div>hello1</div>
+                <div>worlD1</div>
+            </div>
+            <div>
+                <div>hi2</div>
+                <div>hello2</div>
+                <div>worl2</div>
+            </div>
+        </ChildrenProps>
+    */
+
+    render() {
+        console.log( this.props.children );
+        return <>{this.props.children.map(c => <ul> { c.props.children.map(i => <li> {i} </li>) } </ul>)}</>;
+    }
+
+}
+
+export default ChildrenProps;
